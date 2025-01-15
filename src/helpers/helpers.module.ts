09f18489cbe89030aea8c5dd83dd89e15d6d5c4a
tercel <tercel.yi@gmail.com>
2025-01-15 10:25:12 +0800

    Refactor HelpersModule to include SolTransaction provider and export
    - Added SolTransaction as a provider and export in the HelpersModule.
    - This change enhances the module's functionality by making SolTransaction available for dependency injection in other parts of the application.

--- src/helpers/helpers.module.ts
+++ src/helpers/helpers.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { ServicesModule } from '@src/services';
+import { SolTransaction } from '@src/helpers/solana/transaction';
 
 @Module({
   imports: [ServicesModule],
-  providers: [],
-  exports: [],
+  providers: [SolTransaction],
+  exports: [SolTransaction],
 })
 export class HelpersModule {}
